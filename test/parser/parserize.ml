e90dacbc0d8e57561ca07f8834acbc66eee231bd
itorras13 <itorras13@gmail.com>
2016-11-16 16:43:42 -0500

    right now we can only print ints since we do not have type String so changed test function 2. Added semantic check to parserize, and added ast and semantic to makefile for tests

--- test/parser/parserize.ml
+++ test/parser/parserize.ml
@@ -68,5 +68,6 @@ let string_of_program (vars, funcs) =
 let _ =
     let lexbuf = Lexing.from_channel stdin in
     let ast = Parser.program Scanner.token lexbuf in
+    let _ = Semant.check ast in
     let result = string_of_program ast in
     print_string result
